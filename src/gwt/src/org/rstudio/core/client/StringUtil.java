ef71f975f31f79de5d811e3d3dd138ee2658fe6a
JJ Allaire <jj@rstudio.org>
2015-07-02 06:51:53 +0200

    Revert "eliminate eclipse warnings"
    This reverts commit 90817a199c562b13de1745b24d90c6d4352d56f1.

--- src/gwt/src/org/rstudio/core/client/StringUtil.java
+++ src/gwt/src/org/rstudio/core/client/StringUtil.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class StringUtil
 {
--- src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/SetupChunkOptionsPopupPanel.java
+++ src/gwt/src/org/rstudio/studio/client/workbench/views/source/editors/text/SetupChunkOptionsPopupPanel.java
@@ -16,6 +16,7 @@ package org.rstudio.studio.client.workbench.views.source.editors.text;
 
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.inject.Inject;
 
